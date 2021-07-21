8a9cd16044d922e74ed05a1ad36fc912bbf95feb
marcoswmv <marcoswmv@gmail.com>
2021-07-21 16:09:34 +0100

    Initial Commit

---
+++ EasyDelete/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyDelete
+//
+//  Created by Marcos Vicente on 21.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
