1f126e009c4f23bfe46663410d714a70d11ee9aa
marcoswmv <marcoswmv@gmail.com>
2021-08-04 17:31:18 +0100

    delete multiple contacts by selection

--- EasyDelete/ViewControllers/MainViewController.swift
+++ EasyDelete/ViewControllers/MainViewController.swift
@@ -9,15 +9,18 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    typealias ContactSectionsType = [(letter: String, names: [String])]
+    
     private var tableView: UITableView = UITableView()
     
     private var contacts: [String] = ["Marcos Vicente", "Cássia do Carmo", "Walter Morgado", "Danilson Pombal", "Sidney Ribeiro"]
-    private var sections: [(letter: String, names: [String])] = [(letter: String, names: [String])]()
+    private var sections: ContactSectionsType = ContactSectionsType()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureNavigationBar()
+        configureToolbar()
         configureTableView()
     }
     
@@ -27,6 +30,7 @@ class MainViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.ContactsList.cell)
+        tableView.allowsMultipleSelectionDuringEditing = true
         tableView.enableAutoLayout()
         tableView.setConstraints(to: view)
         
@@ -36,45 +40,81 @@ class MainViewController: UIViewController {
     private func configureNavigationBar() {
         navigationItem.title = Constants.mainViewTitle
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Select", style: .done, target: self, action: #selector(handleSelect))
+    }
+    
+    private func configureToolbar() {
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let deleteButton = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(handleDelete))
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(handleDone))
         
+        if !(toolbarItems?.isEmpty ?? true) {
+            toolbarItems?.removeAll()
+        }
+
+        toolbarItems = [deleteButton, flexibleSpace, doneButton]
     }
     
     private func populateData() {
         sections = Helpers.generateSections(from: contacts)
     }
+    
+    // MARK: - Handlers
+
+    @objc private func handleSelect() {
+        navigationItem.rightBarButtonItem?.isEnabled = false
+        navigationController?.setToolbarHidden(false, animated: true)
+        tableView.setEditing(true, animated: true)
+    }
+    
+    @objc private func handleDelete() {
+        if let indexPaths = tableView.indexPathsForSelectedRows {
+            for indexPath in indexPaths.reversed() {
+                self.deleteContact(tableView, at: indexPath)
+            }
+        }
+        
+        navigationItem.rightBarButtonItem?.isEnabled = true
+        navigationController?.setToolbarHidden(true, animated: true)
+        tableView.setEditing(false, animated: true)
+        
+    }
+    
+    @objc private func handleDone() {
+        navigationItem.rightBarButtonItem?.isEnabled = true
+        navigationController?.setToolbarHidden(true, animated: true)
+        tableView.setEditing(false, animated: true)
+        
+    }
 }
 
-// MARK: - Data source
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
 
-extension MainViewController: UITableViewDataSource {
-
+    // MARK: - Data source
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        true
+    }
+    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            sections[indexPath.section].names.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .automatic)
-            print("Row was deleted")
-            
-            if sections[indexPath.section].names.isEmpty {
-                sections.remove(at: indexPath.section)
-                tableView.reloadData()
-                print("Section was deleted")
-            }
+            self.deleteContact(tableView, at: indexPath)
         }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return sections.count
+        return sections.isEmpty ? 1 : sections.count
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections[section].letter
+        return sections.isEmpty ? nil : sections[section].letter
     }
     
     func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        return sections.map { $0.letter }
+        return sections.isEmpty ? nil : sections.map { $0.letter }
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sections[section].names.count
+        return sections.isEmpty ? 0 : sections[section].names.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -85,13 +125,16 @@ extension MainViewController: UITableViewDataSource {
         
         return cell
     }
-}
-
-// MARK: - Delegate
-
-extension MainViewController: UITableViewDelegate {
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
+    // MARK: - Helpers
+    
+    func deleteContact(_ tableView: UITableView, at indexPath: IndexPath) {
+        sections[indexPath.section].names.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+        
+        if sections[indexPath.section].names.isEmpty {
+            sections.remove(at: indexPath.section)
+            tableView.reloadData()
+        }
     }
 }
