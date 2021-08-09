6af6c15a795c96ea1b120069cd40d951ae2249cf
marcoswmv <marcoswmv@gmail.com>
2021-08-09 19:27:09 +0100

    add unit test for helper method: filter, sort

--- EasyDeleteTests/EasyDeleteTests.swift
+++ EasyDeleteTests/EasyDeleteTests.swift
@@ -11,14 +11,30 @@ import XCTest
 
 class EasyDeleteTests: XCTestCase {
 
-    func testGenerateSections() {
+    func testGroupContactsBySections() {
         let contacts: [Contact] = [Contact(givenName: "Marcos", familyName: "Vicente")]
-        let sections = Helpers.generateSections(from: contacts)
+        let sections = Helpers.groupContactsBySections(contacts, deleted: false)
         
-        let (letter, names) = sections.first ?? ("", [Contact(givenName: "", familyName: "")])
+        let (letter, names) = sections.first ?? ("", [Contact(givenName: "", familyName: "", isDeleted: false)])
         
         XCTAssertEqual(letter, String(contacts.first?.givenName.first ?? " "))
         XCTAssertEqual(names, contacts)
     }
+    
+    func testFilter() {
+        let contacts: [Contact] = [Contact(givenName: "Marcos", familyName: "Vicente"),
+                                   Contact(givenName: "Cássia", familyName: "Carmo", isDeleted: true)]
+        let expectedResult = [contacts[0]]
+        
+        XCTAssertEqual(Helpers.filter(contacts, deleted: false), expectedResult)
+    }
 
+    func testSort() {
+        let contacts: [Contact] = [Contact(givenName: "Marcos", familyName: "Vicente"),
+                                   Contact(givenName: "Cássia", familyName: "Carmo")]
+        let expectedResult = [contacts[1],
+                              contacts[0]]
+        
+        XCTAssertEqual(Helpers.sort(contacts), expectedResult)
+    }
 }
