c66fe408b78489b905da6e532d805ee7eb85fa22
marcoswmv <marcoswmv@gmail.com>
2021-09-01 23:51:45 +0100

    - add contact store requests to background queue - stop observing CNContactStoreDidChange, and instead fetch for nnew contacts in the contact store when reloadin g deletedContactsDataSource - reload table views on the main thread - reload data source after contacts recovery

--- EasyDelete/Data/Managers/ContactStoreManager.swift
+++ EasyDelete/Data/Managers/ContactStoreManager.swift
@@ -14,35 +14,17 @@ class ContactStoreManager {
     static let shared = ContactStoreManager()
     
     private let store = CNContactStore()
-    private let notificationCenter = NotificationCenter.default
     private let contactsRequestPublisher = PassthroughSubject<Contact, Error>()
     
-    private init() {
-        notificationCenter.addObserver(forName: .CNContactStoreDidChange, object: nil, queue: .main, using: { [weak self] (_) in
-            DispatchQueue.global(qos: .background).async { [weak self] in
-                guard let self = self else { return }
-                self.requestContacts { [weak self] (result: EDTypes.ContactsRequestResult) in
-                    guard let self = self else { return }
-                    switch result {
-                    case .success(let contact):
-                        self.contactsRequestPublisher.send(contact)
-                    case .failure(let error):
-                        self.contactsRequestPublisher.send(completion: Subscribers.Completion<Error>.failure(error))
-                    }
-                }
-            }
-        })
-    }
-    
-    deinit {
-        print("[Contact store manager] - Deinitializing")
-        notificationCenter.removeObserver(self, name: .CNContactStoreDidChange, object: nil)
-    }
+    private init() { }
     
     func commonDataSourceInit() {
         populateDataSource()
-        requestContacts { [weak self] (result: EDTypes.ContactsRequestResult) in
-            guard let self = self else { return }
+        fetchContactsFromStore()
+    }
+    
+    func fetchContactsFromStore() {
+        self.requestContacts { [unowned self] (result: EDTypes.ContactsRequestResult) in
             switch result {
             case .success(let contact):
                 self.contactsRequestPublisher.send(contact)
@@ -53,43 +35,47 @@ class ContactStoreManager {
     }
     
     private func requestContacts(completionHandler: @escaping EDTypes.ContactsRequestResultHandler) {
-        self.store.requestAccess(for: .contacts) { (granted, error) in
-            
-            if let errorToCatch = error {
-                completionHandler(.failure(errorToCatch))
-            } else if granted {
-                // [Contact Store] Request: Access granted"
+        DispatchQueue.global().async { [unowned self] in
+            store.requestAccess(for: .contacts) { [unowned self] (granted, error) in
                 
-                let keys = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey,
-                            CNContactImageDataKey, CNContactImageDataAvailableKey,
-                            CNContactIdentifierKey, CNContactOrganizationNameKey, CNContactEmailAddressesKey, CNContactJobTitleKey]
-                let request = CNContactFetchRequest(keysToFetch: keys as [CNKeyDescriptor])
-                
-                do {
-                    try self.store.enumerateContacts(with: request, usingBlock: { (contact, _) in
-                        let contact = Contact(contact: contact)
-                        completionHandler(.success(contact))
-                    })
-                } catch {
-                    completionHandler(.failure(error))
+                if let errorToCatch = error {
+                    completionHandler(.failure(errorToCatch))
+                } else if granted {
+                    // [Contact Store] Request: Access granted"
+                    
+                    let keys = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey,
+                                CNContactImageDataKey, CNContactImageDataAvailableKey,
+                                CNContactIdentifierKey, CNContactOrganizationNameKey, CNContactEmailAddressesKey, CNContactJobTitleKey]
+                    let request = CNContactFetchRequest(keysToFetch: keys as [CNKeyDescriptor])
+                    
+                    do {
+                        try self.store.enumerateContacts(with: request, usingBlock: { (contact, _) in
+                            let contact = Contact(contact: contact)
+                            completionHandler(.success(contact))
+                        })
+                    } catch {
+                        completionHandler(.failure(error))
+                    }
+                } else {
+                    // "[Contact Store] Request: Access denied"
                 }
-            } else {
-                // "[Contact Store] Request: Access denied"
             }
         }
     }
     
     private func populateDataSource() {
-        ContactStoreManager.shared.contactsRequestPublisher.sink { errorCompletion in
-            switch errorCompletion {
-            case .failure(let error):
-                Alert.showErrorAlert(on: UIApplication.topViewController()!, message: error.localizedDescription)
-            case .finished:
-                break
+        ContactStoreManager.shared.contactsRequestPublisher
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    Alert.showErrorAlert(on: UIApplication.topViewController()!, message: error.localizedDescription)
+                case .finished:
+                    break
+                }
+            } receiveValue: { contact in
+                DataBaseManager.shared.update(with: contact)
             }
-        } receiveValue: { contact in
-            DataBaseManager.shared.update(with: contact)
-        }.store(in: &Consts.bag)
+            .store(in: &Consts.bag)
     }
     
     func add(contact: Contact) {
@@ -124,10 +110,12 @@ class ContactStoreManager {
         let saveRequest = CNSaveRequest()
         saveRequest.add(newContact, toContainerWithIdentifier: nil)
         
-        do {
-            try store.execute(saveRequest)
-        } catch {
-            Alert.showErrorAlert(on: UIApplication.topViewController()!, message: error.localizedDescription)
+        DispatchQueue.global().async { [unowned self] in
+            do {
+                try self.store.execute(saveRequest)
+            } catch {
+                Alert.showErrorAlert(on: UIApplication.topViewController()!, message: error.localizedDescription)
+            }
         }
     }
     
@@ -135,24 +123,22 @@ class ContactStoreManager {
         let predicate = CNContact.predicateForContacts(withIdentifiers: [identifier])
         let keys = [CNContactIdentifierKey]
         
-        do {
-            let contacts = try store.unifiedContacts(matching: predicate, keysToFetch: keys as [CNKeyDescriptor])
-            guard !contacts.isEmpty else { print("[Contact Store - Delete] No contacts found"); return }
-            guard let contact = contacts.first else { return }
-            
-            let request = CNSaveRequest()
-            // swiftlint:disable:next force_cast
-            let mutableContact = contact.mutableCopy() as! CNMutableContact
-            
-            request.delete(mutableContact)
-            
+        DispatchQueue.global().async { [unowned self] in
             do {
-                try store.execute(request)
-            } catch let err {
-                Alert.showErrorAlert(on: UIApplication.topViewController()!, message: err.localizedDescription)
+                let contacts = try self.store.unifiedContacts(matching: predicate, keysToFetch: keys as [CNKeyDescriptor])
+                guard !contacts.isEmpty else { print("[Contact Store - Delete] No contacts found"); return }
+                guard let contact = contacts.first else { return }
+                
+                let request = CNSaveRequest()
+                // swiftlint:disable:next force_cast
+                let mutableContact = contact.mutableCopy() as! CNMutableContact
+                
+                request.delete(mutableContact)
+                
+                try self.store.execute(request)
+            } catch let error {
+                Alert.showErrorAlert(on: UIApplication.topViewController()!, message: error.localizedDescription)
             }
-        } catch let error {
-            Alert.showErrorAlert(on: UIApplication.topViewController()!, message: error.localizedDescription)
         }
     }
 }
